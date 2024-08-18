b1c1dae0eb49bcd6b7e0de7dc5fa3900039f979b
muchaisam <samson.muchai@strathmore.edu>
2024-08-18 10:35:01 +0300

    feat: Enable view binding and add new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -50,5 +55,12 @@ dependencies {
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //splashscreen
+    implementation("androidx.core:core-splashscreen:1.0.0-rc01")
+
+    //circleimageview
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
 
 }
