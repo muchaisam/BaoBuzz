ed2723e84fda42497033439fe9bce6730bcede85
muchaisam <samson.muchai@strathmore.edu>
2024-08-22 19:25:48 +0300

    feat: Add AppDatabase class for managing the local database

---
+++ app/src/main/java/com/example/baobuzz/daos/AppDatabase.kt
@@ -0,0 +1,46 @@
+package com.example.baobuzz.daos
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.util.Date
+
+@Database(entities = [CachedFixture::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun fixtureDao(): FixtureDao
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "football_database"
+            ).build()
+        }
+    }
+}
+
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
