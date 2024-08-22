c5bf68d40befe297751ba92e046562ee181f28a6
muchaisam <samson.muchai@strathmore.edu>
2024-08-22 19:25:18 +0300

    feat: Add Result sealed class for handling API responses

---
+++ app/src/main/java/com/example/baobuzz/interfaces/Result.kt
@@ -0,0 +1,7 @@
+package com.example.baobuzz.interfaces
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
