01a76d15ed62a0dca1c4c503bd93661e1d4d8d28
muchaisam <samson.muchai@strathmore.edu>
2024-08-23 05:49:57 +0300

    refactor: Update FixtureDetails model to include referee, timezone, timestamp, periods, venue, and status properties

--- app/src/main/java/com/example/baobuzz/models/FixtureDetails.kt
+++ app/src/main/java/com/example/baobuzz/models/FixtureDetails.kt
@@ -1,7 +1,15 @@
 package com.example.baobuzz.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FixtureDetails(
     val id: Int,
+    val referee: String?,
+    val timezone: String,
     val date: String,
-    val status: FixtureStatus
+    val timestamp: Long,
+    val periods: Periods,
+    val venue: Venue,
+    val status: Status
 )
