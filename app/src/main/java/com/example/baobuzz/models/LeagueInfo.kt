44c9a6d698dc8e3c1847f52439286a1197164fbc
muchaisam <samson.muchai@strathmore.edu>
2024-08-23 05:49:37 +0300

    refactor: Add id property to LeagueInfo data class

--- app/src/main/java/com/example/baobuzz/models/LeagueInfo.kt
+++ app/src/main/java/com/example/baobuzz/models/LeagueInfo.kt
@@ -1,3 +1,3 @@
 package com.example.baobuzz.models
 
-data class LeagueInfo(val name: String, val country: String, val flagUrl: String)
+data class LeagueInfo(val id: Int, val name: String, val country: String, val flagUrl: String)
