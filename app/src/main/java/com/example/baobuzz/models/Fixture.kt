603fe6cb12e53780591458c4bb8974dac247386d
muchaisam <samson.muchai@strathmore.edu>
2024-08-23 05:48:59 +0300

    feat: Add goals property to Fixture data class

--- app/src/main/java/com/example/baobuzz/models/Fixture.kt
+++ app/src/main/java/com/example/baobuzz/models/Fixture.kt
@@ -1,7 +1,11 @@
 package com.example.baobuzz.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Fixture(
     val fixture: FixtureDetails,
     val league: League,
-    val teams: Teams
+    val teams: Teams,
+    val goals: Goals
 )
