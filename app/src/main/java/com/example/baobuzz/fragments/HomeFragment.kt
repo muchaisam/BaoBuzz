732653f926bc67a3be1856b4eb4f4b1d0c8fc0cb
muchaisam <samson.muchai@strathmore.edu>
2024-08-26 12:32:36 +0300

    refactor: Update HomeFragment to use ViewModel and RecyclerView for displaying league information

--- app/src/main/java/com/example/baobuzz/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/baobuzz/fragments/HomeFragment.kt
@@ -5,56 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.baobuzz.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.baobuzz.adapter.LeagueAdapter
+import com.example.baobuzz.databinding.FragmentHomeBinding
+import com.example.baobuzz.models.HomeViewModel
+import com.example.baobuzz.models.HomeViewModelFactory
+import com.example.baobuzz.models.LeagueInfoProvider
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var leagueAdapter: LeagueAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this, HomeViewModelFactory(LeagueInfoProvider()))
+            .get(HomeViewModel::class.java)
+
+        setupLeagueRecyclerView()
+        observeLeagues()
+    }
+
+    private fun setupLeagueRecyclerView() {
+        leagueAdapter = LeagueAdapter()
+        binding.rvLeagues.apply {
+            adapter = leagueAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun observeLeagues() {
+        viewModel.leagues.observe(viewLifecycleOwner) { leagues ->
+            leagueAdapter.submitList(leagues)
+        }
     }
 }
+
