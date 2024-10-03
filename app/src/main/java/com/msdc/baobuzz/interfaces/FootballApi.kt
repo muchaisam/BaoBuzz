2fafe6120e9178b47a51b7f1081da885b17fc94b
muchaisam <samson.muchai@strathmore.edu>
2024-10-03 09:02:09 +0300

    Refactor FootballApi interface to add new methods for retrieving leagues and teams

--- app/src/main/java/com/msdc/baobuzz/interfaces/FootballApi.kt
+++ app/src/main/java/com/msdc/baobuzz/interfaces/FootballApi.kt
@@ -3,7 +3,11 @@ package com.msdc.baobuzz.interfaces
 import com.msdc.baobuzz.models.ApiResponse
 import com.msdc.baobuzz.models.Coach
 import com.msdc.baobuzz.models.FixturesResponse
+import com.msdc.baobuzz.models.League
+import com.msdc.baobuzz.models.LeagueResponse
 import com.msdc.baobuzz.models.StandingsResponse
+import com.msdc.baobuzz.models.Team
+import com.msdc.baobuzz.models.TeamResponse
 import com.msdc.baobuzz.models.TransfersResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -29,4 +33,19 @@ interface FootballApi {
     @GET("coachs")
     suspend fun getCoach(@Query("id") id: Int): ApiResponse<Coach>
 
+    // New methods
+    @GET("leagues")
+    suspend fun getLeagues(@Query("current") current: Boolean): LeagueResponse
+
+    @GET("leagues")
+    suspend fun searchLeagues(@Query("search") query: String): LeagueResponse
+
+    @GET("teams")
+    suspend fun getTeams(
+        @Query("league") league: Int,
+        @Query("season") season: Int
+    ): TeamResponse
+
+    @GET("teams")
+    suspend fun searchTeams(@Query("search") query: String): TeamResponse
 }
