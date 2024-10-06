473f052989ec3bb4e9d62adec7c2ac8912f5735e
muchaisam <samson.muchai@strathmore.edu>
2024-10-06 03:05:24 +0300

    Refactor Theme.kt to use custom font family and adjust typography sizes

--- app/src/main/java/com/msdc/baobuzz/ux/Theme.kt
+++ app/src/main/java/com/msdc/baobuzz/ux/Theme.kt
@@ -1,25 +1,47 @@
 package com.msdc.baobuzz.ux
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.msdc.baobuzz.R
+
+@Composable
+fun FootballAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+//        colors = lightColors(), // Or darkColors() for a dark theme
+        typography = Typography(),
+        shapes = Shapes(),
+        content = content
+    )
+}
+
+// Declare the font families
+
+val Inter = FontFamily(
+    Font(R.font.inter),
+)
+
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 17.sp
     ),
     body2 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     h5 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp
+        fontSize = 20.sp
     )
 )
