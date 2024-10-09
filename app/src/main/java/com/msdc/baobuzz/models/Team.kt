419ee97af42651c261cf00c9949061fe8513de41
muchaisam <samson.muchai@strathmore.edu>
2024-10-09 22:25:33 +0300

    refactor: Add database relationship field to Team model

--- app/src/main/java/com/msdc/baobuzz/models/Team.kt
+++ app/src/main/java/com/msdc/baobuzz/models/Team.kt
@@ -1,11 +1,19 @@
 package com.msdc.baobuzz.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Entity(tableName = "teams")
 data class Team(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val name: String,
+    val code: String?,
+    val country: String,
+    val founded: Int?,
+    val national: Boolean,
     val logo: String,
-    val winner: Boolean?
+    // Add this field for database relationship
+    var leagueId: Int = 0  // Default value of 0
 )
