5d088b79df2af50e92632e2bfb037ef3211d75a7
muchaisam <samson.muchai@strathmore.edu>
2024-11-02 21:18:38 +0300

    feat: Add ThemeState class for managing theme mode toggling

---
+++ app/src/main/java/com/msdc/baobuzz/ui/theme/ThemeState.kt
@@ -0,0 +1,18 @@
+package com.msdc.baobuzz.ui.theme
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
+class ThemeState(
+    initialThemeMode: ThemeMode = ThemeMode.Light
+) {
+    private val _themeMode = mutableStateOf(initialThemeMode)
+    val themeMode: State<ThemeMode> = _themeMode
+
+    fun toggleTheme() {
+        _themeMode.value = when (themeMode.value) {
+            ThemeMode.Light -> ThemeMode.Dark
+            ThemeMode.Dark -> ThemeMode.Light
+        }
+    }
+}
