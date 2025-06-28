d0c0f906841789f99f25b3103e32c313a997a26a
muchaisam <samson.muchai@strathmore.edu>
2025-06-28 21:59:51 +0300

    refactor: Remove unused navigation components and screens

--- app/src/main/java/com/msdc/baobuzz/appnavigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.msdc.baobuzz.appnavigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Competition : Screen("competition")
-    object Statistics : Screen("statistics")
-    object Settings : Screen("settings")
-}
