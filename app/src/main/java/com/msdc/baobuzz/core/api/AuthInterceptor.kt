a3570a29c940cb71fd7c7af28dd8d052b9dc4cca
muchaisam <samson.muchai@strathmore.edu>
2025-07-06 07:52:09 +0300

    feat: Add AuthInterceptor to include authentication headers in API requests

---
+++ app/src/main/java/com/msdc/baobuzz/core/api/AuthInterceptor.kt
@@ -0,0 +1,21 @@
+package com.msdc.baobuzz.core.api
+
+import com.msdc.baobuzz.BuildConfig
+import javax.inject.Inject
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/** Interceptor that adds authentication headers to API requests */
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+        val requestBuilder =
+                original.newBuilder()
+                        .header("x-rapidapi-key", BuildConfig.FOOTBALL_API_KEY)
+                        .header("x-rapidapi-host", "v3.football.api-sports.io")
+
+        val request = requestBuilder.build()
+        return chain.proceed(request)
+    }
+}
