49c017e08a617f597580f7832690d52c46d11abd
muchaisam <samson.muchai@strathmore.edu>
2025-07-06 09:49:43 +0300

    feat: Add navigation routes for the app

---
+++ app/src/main/java/com/msdc/baobuzz/core/navigation/BaoBuzzRoutes.kt
@@ -0,0 +1,20 @@
+package com.msdc.baobuzz.core.navigation
+
+/**
+ * Navigation routes for the app
+ */
+object BaoBuzzRoutes {
+    const val SPLASH = "splash"
+    const val ONBOARDING_WELCOME = "onboarding_welcome"
+    const val ONBOARDING_FEATURES = "onboarding_features"
+    const val ONBOARDING_LEAGUES = "onboarding_leagues"
+    const val ONBOARDING_TEAMS = "onboarding_teams"
+    const val MAIN_APP = "main_app"
+
+    // Main app destinations
+    const val HOME = "home"
+    const val LEAGUES = "leagues"
+    const val STATS = "stats"
+    const val SETTINGS = "settings"
+
+}
