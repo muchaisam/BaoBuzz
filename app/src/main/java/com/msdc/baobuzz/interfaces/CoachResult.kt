b523f4cee910daeae9fa7a6b5e68c20fa0f96ba7
muchaisam <samson.muchai@strathmore.edu>
2025-07-06 09:54:50 +0300

    fix: Update Error data class to use Nothing type for consistency in CoachResult sealed class

--- app/src/main/java/com/msdc/baobuzz/interfaces/CoachResult.kt
+++ app/src/main/java/com/msdc/baobuzz/interfaces/CoachResult.kt
@@ -2,5 +2,6 @@ package com.msdc.baobuzz.interfaces
 
 sealed class CoachResult<out T> {
     data class Success<out T>(val data: T) : CoachResult<T>()
-    data class Error<T>(val exception: Exception) : CoachResult<T>()
+    data class Error(val exception: Exception) : CoachResult<Nothing>()
+    object RateLimit : CoachResult<Nothing>()
 }
