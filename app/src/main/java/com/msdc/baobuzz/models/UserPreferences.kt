0f6b2ade1d5a023f790845095b72ae8a63709215
muchaisam <samson.muchai@strathmore.edu>
2025-07-06 09:54:21 +0300

    feat: Update UserPreferences data class to include default values for fields

--- app/src/main/java/com/msdc/baobuzz/models/UserPreferences.kt
+++ app/src/main/java/com/msdc/baobuzz/models/UserPreferences.kt
@@ -1,7 +1,10 @@
 package com.msdc.baobuzz.models
 
 data class UserPreferences(
-    val selectedLeagueIds: List<Int>,
-    val selectedTeamIds: List<Int>,
-    val teamNotifications: Map<Int, Boolean>
+    val selectedLeagueIds: List<Int> = emptyList(),
+    val selectedTeamIds: List<Int> = emptyList(),
+    val teamNotifications: Map<Int, Boolean> = emptyMap(),
+    val isOnboardingCompleted: Boolean = false,
+    val preferredLanguage: String = "en",
+    val notificationsEnabled: Boolean = true
 )
