24d2a60908387df947e9a871cc1f053df3911e17
muchaisam <samson.muchai@strathmore.edu>
2025-07-06 09:53:59 +0300

    feat: Update League model to include nullable fields and primary key annotation

--- app/src/main/java/com/msdc/baobuzz/models/League.kt
+++ app/src/main/java/com/msdc/baobuzz/models/League.kt
@@ -1,14 +1,18 @@
 package com.msdc.baobuzz.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Entity(tableName = "leagues")
 data class League(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val name: String,
-    val country: String,
+    val type: String,
+    val country: String?,
     val logo: String,
-    val flag: String,
-    val season: Int,
-    val round: String
+    val flag: String?,
+    val season: Int?,
+    val round: String?
 )
