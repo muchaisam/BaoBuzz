b18e8619e68242d1e261c191231b187a26d6f152
muchaisam <samson.muchai@strathmore.edu>
2025-09-30 20:17:07 +0300

    fix: Update Hilt Gradle plugin version to 2.48.1 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,6 @@ allprojects {
 
 buildscript {
     dependencies {
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
     }
 }
