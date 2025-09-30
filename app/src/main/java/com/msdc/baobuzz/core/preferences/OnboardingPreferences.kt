47f9b1fd30bcd447fc15d06374c5854e3663f013
muchaisam <samson.muchai@strathmore.edu>
2025-09-30 20:03:53 +0300

    refactor: Remove unused Serializable import from OnboardingPreferences

--- app/src/main/java/com/msdc/baobuzz/core/preferences/OnboardingPreferences.kt
+++ app/src/main/java/com/msdc/baobuzz/core/preferences/OnboardingPreferences.kt
@@ -8,7 +8,6 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.msdc.baobuzz.core.models.UserPreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
