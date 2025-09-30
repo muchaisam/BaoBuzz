4262f28f874e0f7fbad94a2c643ae4cd825b9aa9
muchaisam <samson.muchai@strathmore.edu>
2025-09-30 20:02:27 +0300

    refactor: Update ApiClient to use BuildConfig for API key

--- app/src/main/java/com/msdc/baobuzz/api/ApiClient.kt
+++ app/src/main/java/com/msdc/baobuzz/api/ApiClient.kt
@@ -1,31 +1,39 @@
 package com.msdc.baobuzz.api
 
+import com.msdc.baobuzz.BuildConfig
 import com.msdc.baobuzz.interfaces.FootballApi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/**
+ * Legacy ApiClient - will be replaced by new DI-based NetworkModule TODO: Remove after migration to
+ * new architecture
+ */
+@Deprecated("Use NetworkModule with Hilt injection instead")
 object ApiClient {
     private const val BASE_URL = "https://v3.football.api-sports.io/"
-    private const val API_KEY = "8a40b9da2da68f7f308bced6d7e91f62"
     private const val API_HOST = "v3.football.api-sports.io"
 
-    private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor { chain ->
-            val original = chain.request()
-            val requestBuilder = original.newBuilder()
-                .header("x-rapidapi-key", API_KEY)
-                .header("x-rapidapi-host", API_HOST)
-            val request = requestBuilder.build()
-            chain.proceed(request)
-        }
-        .build()
+    private val okHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val requestBuilder =
+                    original.newBuilder()
+                        .header("x-rapidapi-key", BuildConfig.FOOTBALL_API_KEY)
+                        .header("x-rapidapi-host", API_HOST)
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            .build()
 
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    val retrofit: Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
     val footballApi: FootballApi = retrofit.create(FootballApi::class.java)
 }
