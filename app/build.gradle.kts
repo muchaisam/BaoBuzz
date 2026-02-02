a023fcf7e88752f4f4dc7022bf497fac1fe5fc98
muchaisam <samson.muchai@strathmore.edu>
2026-02-02 11:16:45 +0300

    feat: Migrate from kapt to ksp for annotation processing and update Kotlin serialization plugin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    id ("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
     id("dagger.hilt.android.plugin")
 }
 
@@ -28,8 +28,9 @@ android {
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // Add API key from local.properties
+        // Add API keys from local.properties
         buildConfigField("String", "FOOTBALL_API_KEY", "\"${localProperties.getProperty("API_KEY") ?: ""}\"")
+        buildConfigField("String", "FOOTBALL_DATA_API_KEY", "\"${localProperties.getProperty("FOOTBALL_DATA_API_KEY") ?: ""}\"")
     }
 
     buildTypes {
@@ -67,13 +68,6 @@ android {
         compose = true
     }
 
-    kapt {
-        correctErrorTypes = true
-        showProcessorStats = true
-        arguments {
-            arg("kapt.verbose", "true")
-        }
-    }
 }
 
 dependencies {
@@ -113,11 +107,9 @@ dependencies {
     // Material Design
     implementation("com.google.android.material:material:1.11.0")
 
-    // Navigation
+    // Navigation - Pure Compose
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
@@ -127,14 +119,14 @@ dependencies {
 
     // Hilt - Compatible with Kotlin 1.9.0
     implementation("com.google.dagger:hilt-android:$hiltVersion")
-    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
     implementation("androidx.hilt:hilt-work:1.1.0")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    ksp("androidx.hilt:hilt-compiler:1.1.0")
 
     // Room - Compatible with Kotlin 1.9.0
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
-    kapt("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
@@ -153,7 +145,7 @@ dependencies {
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    kapt("com.github.bumptech.glide:compiler:4.16.0")
+    ksp("com.github.bumptech.glide:compiler:4.16.0")
 
     // Kotlin DateTime - Compatible with Kotlin 1.9.0
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
