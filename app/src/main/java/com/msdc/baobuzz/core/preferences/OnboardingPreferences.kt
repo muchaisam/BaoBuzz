230e8d29a7a4618672994da3512a368f81774f1b
muchaisam <samson.muchai@strathmore.edu>
2026-02-13 21:41:59 +0300

    fix: Remove unused import for decodeFromString in OnboardingPreferences

--- app/src/main/java/com/msdc/baobuzz/core/preferences/OnboardingPreferences.kt
+++ app/src/main/java/com/msdc/baobuzz/core/preferences/OnboardingPreferences.kt
@@ -8,7 +8,6 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.msdc.baobuzz.core.models.UserPreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
