a0af64a624b09c36e25d55e52c6f19249bbb5df4
muchaisam <samson.muchai@strathmore.edu>
2026-02-14 17:11:09 +0300

    style: Update build.gradle.kts for improved dependency management and add KSP support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,29 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+buildscript {
+    val kotlinVersion by extra("1.9.20")
+    val dagger_version by extra("2.40.5")
+    val kspVersion by extra("1.9.0-1.0.13") // Add KSP version
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:$kspVersion") // Add KSP
+    }
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-
-buildscript {
-    dependencies {
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
-    }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
