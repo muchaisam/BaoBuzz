f3291961bc3428b0c11ca6ef12ebd85204619185
muchaisam <samson.muchai@strathmore.edu>
2026-02-15 17:04:20 +0300

    refactor: Remove unused result interfaces to streamline codebase

--- app/src/main/java/com/msdc/baobuzz/interfaces/CoachResult.kt
+++
@@ -1,7 +0,0 @@
-package com.msdc.baobuzz.interfaces
-
-sealed class CoachResult<out T> {
-    data class Success<out T>(val data: T) : CoachResult<T>()
-    data class Error(val exception: Exception) : CoachResult<Nothing>()
-    object RateLimit : CoachResult<Nothing>()
-}
